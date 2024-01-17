c398f04fb66a0ccd5c3e237d780a9e43d77dc9b4
MunibaAtiq <munibaatiqaptech@gmail.com>
2024-01-17 11:35:38 +0500

    Operators work done!

---
+++ RelationalOprator.js
@@ -0,0 +1,52 @@
+// x = "15";//= Assignment operator
+// y = "15";
+//conditional statements if else
+//== Equal to Operator
+// if(x === y){
+//     document.write("x is equal to y");
+// }
+// else{
+//     document.write("x is Not equals to y");
+// }
+// //=== strict equal operator
+
+//not equal !=
+// p = 20;
+// q = "20";
+// //20 != 20
+// if(p !== q){
+//     document.write("True");
+// }else{
+//     document.write("False");
+// }
+//strict not equal !==
+
+//greater than >
+// x = 5;
+// y = 6;
+// //5>6
+// if(x>y){
+//     document.write("x is greater than y");
+// }
+// else{
+//     document.write("x is not greater than y");
+// }
+//less than <
+// x = 5;
+// y = 6;
+// //5 < 6
+// if(x < y){
+//     document.write('x is less than y');
+// }
+// else{
+//     document.write('x is not less than y');
+// }
+//Greater than equal to >=
+// x = 16;
+// y = 15;
+// if(x >= y){
+//     document.write("True");
+// }
+// else{
+//     document.write("flase");
+// }
